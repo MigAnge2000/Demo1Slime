139b23f30682ad9324a1c3705e96809cbf13f6bc
MigAnge2000 <18cs0307@cdserdan.tecnm.mx>
2022-12-26 15:21:28 -0600

    Subir Demo de Slide

---
+++ SlideDemo/SlideDemo/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using SlideDemo.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SlideDemo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new FatherPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ SlideDemo/SlideDemo/Views/SonPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+
+namespace SlideDemo.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SonPage : PopupPage
+    {
+        public SonPage()
+        {
+            InitializeComponent();
+        }
+        public double Verticaltransition { get; set; }
+
+
+        private async void PanGestureRecognizer_PanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            switch (e.StatusType)
+            {
+                case GestureStatus.Running:
+                    if (e.TotalY > 0)
+                    {
+                        await gridprincipal.TranslateTo(0, e.TotalY);
+                        Verticaltransition = e.TotalY;
+                    }
+                    break;
+                    case GestureStatus.Completed:
+                    if (Verticaltransition > 100)
+                    {
+                        await gridprincipal.TranslateTo(0,200);
+                       if(PopupNavigation.Instance.PopupStack.Any())
+                        {
+                            await PopupNavigation.Instance.PopAsync();
+                        }
+                    }
+                    else
+                    {
+                        await gridprincipal.TranslateTo(0, e.TotalY);
+                    }
+                    break;
+
+            }
+            lblY.Text = e.TotalY.ToString();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            PopupNavigation.Instance.PopAsync();
+        }
+    }
+}
